6336b5f1971bd2c6c4d13156ddf871502a057682
adolfoval <adolfovalvid@gmail.com>
2021-11-15 19:29:16 -0500

    cambios

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <script src = "https://kit.fontawesome.com/d4cd33a1a3.js" crossOrigin ="anonymous"/>
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default MyDocument
