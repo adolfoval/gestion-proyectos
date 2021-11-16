2580f5deda3faecbd8fbf5811c9661c033a77272
adolfoval <adolfovalvid@gmail.com>
2021-11-15 19:42:08 -0500

    script corregido

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,25 @@
-import Document, {DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import Script from "next/script"
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <Script src="https://kit.fontawesome.com/d4cd33a1a3.js" crossOrigin="anonymous" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
 export default MyDocument
