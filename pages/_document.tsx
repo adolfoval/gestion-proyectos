92ed0c543795dc9916e757e6c0ade6452a8f0c88
adolfoval <adolfovalvid@gmail.com>
2021-11-15 19:38:59 -0500

    correccion de error script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Script from "next/script"
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
@@ -10,7 +11,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <script src = "https://kit.fontawesome.com/d4cd33a1a3.js" crossOrigin ="anonymous"/>
+                    <Script src = "https://kit.fontawesome.com/d4cd33a1a3.js" crossOrigin ="anonymous"/>
                 </Head>
                 <body>
                     <Main />
